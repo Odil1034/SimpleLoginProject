216268b0c2744ac83bed1e621db95f9ce03d0017
Odiljon <odiljonbaxriddinov735@gmail.com>
2024-09-17 21:09:35 +0500

    created spring boot application

---
+++ src/test/java/uz/pdp/SimpleLoginProject/SimpleLoginProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package uz.pdp.SimpleLoginProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleLoginProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
